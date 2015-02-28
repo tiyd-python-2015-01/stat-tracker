3bbda59378fe3d04f7f5750816ec1598c9d41670
johnpmitsch <johnpmitsch@gmail.com>
2015-02-28 11:55:51 -0500

    can't submit activity

--- stat_tracker/views/stats.py
+++ stat_tracker/views/stats.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, flash, render_template
+from flask import Blueprint, flash, render_template, redirect, url_for, request
+from flask.ext.login import login_required, current_user
+from ..forms import ActivityForm
+
+from ..extensions import db
+from ..models import Activity
 
 
 stats = Blueprint("stats", __name__)
@@ -15,4 +20,18 @@ def flash_errors(form, category="warning"):
 @stats.route("/")
 def index():
     return render_template('index.html')
+
+@stats.route("/activity", methods=['GET', 'POST'])
+@login_required
+def add_activity():
+    form = ActivityForm()
+    if form.validate_on_submit():
+        activity = Activity(name=form.name.data,
+                            user_id=current_user.id)
+        db.session.add(activity)
+        db.session.commit()
+        flash("Activity has been added")
+        return redirect(url_for("stats.index"))
+    return render_template("activity.html", form=form)
+
 
