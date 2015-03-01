3db72c431c23b16281ef1a821b04152f06b2d9e1
johnpmitsch <johnpmitsch@gmail.com>
2015-02-28 22:25:56 -0500

    added d3 and it rocks

--- stat_tracker/views/stats.py
+++ stat_tracker/views/stats.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, flash, render_template, redirect, url_for, request
 from flask.ext.login import login_required, current_user
-from ..forms import ActivityForm
+from ..forms import ActivityForm, StatForm
+from datetime import date
 
 from ..extensions import db
-from ..models import Activity
+from ..models import Activity, Stat
 
 
 stats = Blueprint("stats", __name__)
@@ -19,7 +20,9 @@ def flash_errors(form, category="warning"):
 
 @stats.route("/")
 def index():
-    return render_template('index.html')
+    activities = Activity.query.all()
+    data = [[stat.value for stat in activity.stats] for activity in activities]
+    return render_template('index.html', activities=activities, data=data)
 
 @stats.route("/activity", methods=['GET', 'POST'])
 @login_required
@@ -27,11 +30,56 @@ def add_activity():
     form = ActivityForm()
     if form.validate_on_submit():
         activity = Activity(name=form.name.data,
-                            user_id=current_user.id)
+                            user_id=current_user.id,
+                            unit=form.unit.data)
         db.session.add(activity)
         db.session.commit()
         flash("Activity has been added")
         return redirect(url_for("stats.index"))
     return render_template("activity.html", form=form)
+
+@stats.route("/stats", methods=['GET', 'POST'])
+@login_required
+def add_stat():
+    form = StatForm()
+    form.activity.choices = [(activity.id, activity.name + " - "  + activity.unit) for activity in Activity.query.all()]
+    print("add_stat ran")
+    if form.validate_on_submit():
+        print("validating, yo")
+        activity_id = form.activity.data
+        activity = Activity.query.get(activity_id)
+        stat = Stat(user_id=current_user.id,
+                    activity_id=activity.id,
+                    date=form.date.data,
+                    value=form.value.data)
+        db.session.add(stat)
+        db.session.commit()
+        flash("Stat Attack has added your stat!")
+
+    return render_template("stats.html", form=form,
+                                         button='submit',
+                                         post_url=url_for("stats.add_stat"))
+
+@stats.route("/stats/<int:id>/edit", methods=['GET', 'POST'])
+@login_required
+def edit_stat(id):
+    stat = Stat.query.get(id)
+    form = StatForm(obj=stat)
+    activity_id = stat.activity_id
+    activity = Activity.query.get(activity_id)
+    if request.method == 'POST':
+        form.populate_obj(stat)
+        stat.date = form.date.data
+        stat.value = form.value.data
+        db.session.add(stat)
+        db.session.commit()
+        flash("Stat Attack has updated your stat!")
+    return render_template("edit.html", form=form,
+                                         button="edit",
+                                         activity=activity,
+                                         post_url=url_for("stats.edit_stat", id=stat.id))
+
+
+
 
 
